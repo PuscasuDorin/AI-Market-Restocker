49352402a363dacb871ac8f049bc5453e0c71cee
Puscasu Dorin <puscasudorin20052@gmail.com>
2025-05-26 17:33:44 +0300

    py code updated

--- AI_Sorter/main.py
+++ AI_Sorter/main.py
@@ -5,62 +5,74 @@ from bluepy import btle
 from gpiozero import Button
 import time
 
-# Configurare Bluetooth
-DEVICE_MAC = "F4:12:FA:70:AA:C5"  # MAC-ul Arduino-ului tău
+#Bluetooth
+DEVICE_MAC = "F4:12:FA:70:AA:C5"
 SERVICE_UUID = "180C"
 CHAR_UUID = "2A56"
 message = ""
 
-# Așteaptă ca Arduino să înceapă publicarea BLE
-print("Aștept 5 secunde ca Arduino să pornească BLE...")
-time.sleep(5)
-
-# Conectare Bluetooth
-try:
-    print("Conectare la Arduino...")
-    peripheral = btle.Peripheral(DEVICE_MAC)
-    service = peripheral.getServiceByUUID(SERVICE_UUID)
-    characteristic = service.getCharacteristics(CHAR_UUID)[0]
-    print("Conectat la Arduino!")
-except Exception as e:
-    print(f"Eroare de conexiune Bluetooth: {e}")
-    exit(1)
-
-# Setare camera
+# Camera setup
 picam2 = Picamera2()
 picam2.preview_configuration.main.size = (750, 750)
 picam2.preview_configuration.main.format = "RGB888"
 picam2.preview_configuration.align()
 picam2.configure("preview")
-picam2.start()
 
-# Încarcă modelul YOLO
+
+print("Waiting to connect...")
+time.sleep(3)
+
+try:
+    print("Connecting...")
+    peripheral = btle.Peripheral(DEVICE_MAC)
+    service = peripheral.getServiceByUUID(SERVICE_UUID)
+    characteristic = service.getCharacteristics(CHAR_UUID)[0]
+    print("Connected")
+    picam2.start()
+    picam2.capture_array()
+
+except Exception as e:
+    print(f"Bluetooth Connection Error: {e}")
+    exit(1)
+
+
+# Load YOLO model
 model = YOLO("AI_Sorter/my_model_ncnn_model")
 
-# Bucla principală
+# Start
 while True:
-    # Capture frame
+
     frame = picam2.capture_array()
     
-    # Rulează YOLO pe frame
     results = model(frame)
     annotated_frame = results[0].plot()
 
-    # Dacă sunt detecții
+    # Dettection check
     if results[0].boxes is not None and results[0].boxes.cls is not None:
         classes = results[0].boxes.cls
         labels = [model.names[int(cls)] for cls in classes]
-        print(f"Detectat: {labels}")
+        print(f"Dettected: {labels}")
 
-        # Trimite eticheta către Arduino
+        # Sending data via bl to ble
         try:
             message = labels[0]
-            print(f"Trimit către Arduino: {message}")
+            if message == "Red":
+                message = 'R'
+            elif message == "Green":
+                message = 'G'
+            elif message == "Yellow":
+                message = 'Y'
+            elif message == "Blue":
+                message = 'B'
+            elif message == "Orange":
+                message = 'O'
+            print(f"Sending message: {message}")
             characteristic.write(message.encode(), withResponse=True)
+            time.sleep(2)
         except Exception as e:
-            print(f"Eroare la trimiterea către Arduino: {e}")
+            print(f"Error sending message: {e}")
     else:
-        print("Nicio detecție.")
+        print("No Detection.")
 
     # FPS
     inference_time = results[0].speed['inference']
@@ -74,12 +86,11 @@ while True:
 
     cv2.putText(annotated_frame, text, (text_x, text_y), font, 1, (255, 255, 255), 2, cv2.LINE_AA)
 
-    # Afișează frame-ul
     cv2.imshow("Camera", annotated_frame)
 
-    # Ieșire dacă se apasă 'q'
+    # QUIT CAM - 'q'
     if cv2.waitKey(1) == ord("q"):
         break
 
-# Curățare
+# OBJ Search
 cv2.destroyAllWindows()
